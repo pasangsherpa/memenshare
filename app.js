c6d1bf59f7b2f450bcd981c915139ac7e25d975a
Pasang Sherpa <pasang_sh@hotmail.com>
2015-12-28 23:07:13 -0800

    meme schema added

--- app.js
+++ app.js
@@ -6,16 +6,11 @@
 
 const compress = require('koa-compress');
 const convert = require('koa-convert');
+const requireDir = require('require-directory');
 const mongoose = require('mongoose');
 const co = require('co');
 const Koa = require('koa');
 const app = new Koa();
-
-/**
- * Load routes.
- */
-
-const api = require('./routes/api');
 
 /**
  * Environment variables.
@@ -26,11 +21,20 @@ const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/memenshare';
 
 /**
+ * Load routes.
+ */
+
+const api = require('./routes/api');
+
+/**
  * Connect to database
  */
 
 mongoose.connect(dbUrl);
 mongoose.connection.on('error', err => console.log(err));
+
+// load mongoose models
+requireDir(module, './models');
 
 // logging
 if (env !== 'production') {
---
+++ models/meme.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * Schema
+ */
+
+let memeSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  meta: {
+    likes: Number
+  }
+});
+
+// model creation
+mongoose.model('Meme', memeSchema);
