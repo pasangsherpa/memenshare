e096180620d1cfbe7e89de5db0507f0f2ce73320
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-29 22:47:30 -0800

    changed jsonapi serializer

--- controllers/meme.go
+++ controllers/meme.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/github.com/gin-gonic/gin"
-	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/github.com/pasangsherpa/jsonapi"
+	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/github.com/manyminds/api2go/jsonapi"
 	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2"
 	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
 	"github.com/pasangsherpa/memenshare/models"
@@ -15,6 +16,8 @@ type (
 		collection *mgo.Collection
 	}
 )
+
+const linkTemplateMemes = "http://localhost:3000/memes"
 
 // NewMemeController provides a reference to a MemeController
 // with provided mongo collection
@@ -32,17 +35,30 @@ func (mc MemeController) GetMemes(c *gin.Context) {
 		return
 	}
 
-	memeInterface := make([]interface{}, len(memes))
+	// memeInterface := make([]interface{}, len(memes))
 
-	for i, meme := range memes {
-		memeInterface[i] = &meme
-	}
+	// for i, meme := range memes {
+	// 	memeInterface[i] = &meme
+	// }
+
+	fmt.Printf("memes %+v\n", memes)
 
 	c.Writer.Header().Set("Content-Type", "application/vnd.api+json")
-	if err := jsonapi.MarshalManyPayload(c.Writer, memeInterface); err != nil {
+
+	marshalResult, err := jsonapi.Marshal(memes)
+
+	fmt.Printf("marshalResult %+v\n", marshalResult)
+
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
 		return
 	}
+
+	c.JSON(http.StatusOK, marshalResult)
+	// if err := jsonapi.MarshalManyPayload(c.Writer, memeInterface); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, err)
+	// 	return
+	// }
 }
 
 func (mc MemeController) GetMeme(c *gin.Context) {
@@ -67,8 +83,23 @@ func (mc MemeController) GetMeme(c *gin.Context) {
 		return
 	}
 
+	fmt.Printf("meme %+v\n", meme)
+
 	c.Writer.Header().Set("Content-Type", "application/vnd.api+json")
-	if err := jsonapi.MarshalOnePayload(c.Writer, meme); err != nil {
+
+	marshalResult, err := jsonapi.Marshal(meme)
+
+	// fmt.Printf("marshalResult %+v\n", marshalResult)
+
+	if err != nil {
 		c.JSON(http.StatusInternalServerError, err)
+		return
 	}
+
+	c.JSON(http.StatusOK, marshalResult)
+	// if err := jsonapi.MarshalOnePayloadWithExtras(c.Writer, meme, func(extras *jsonapi.ApiExtras) {
+	// 	extras.AddRootLink("self", linkTemplateMemes+"/"+id)
+	// }); err != nil {
+	// 	c.JSON(http.StatusInternalServerError, err)
+	// }
 }
--- models/meme.go
+++ models/meme.go
@@ -1,16 +1,33 @@
 package models
 
-import (
-	"time"
+import "github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
 
-	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
-)
+// type Meme struct {
+// 	Id        bson.ObjectId     `json:"id" bson:"_id" jsonapi:"primary,memes"`
+// 	Links     map[string]string `jsonapi:"links,top"`
+// 	CreatedAt time.Time         `json:"createdAt" bson:"createdAt" jsonapi:"attr,createdAt"`
+// 	UpdatedAt time.Time         `json:"updatedAt" bson:"updatedAt" jsonapi:"attr,updatedAt"`
+// 	Title     string            `json:"title" jsonapi:"attr,title"`
+// 	Author    string            `json:"author" jsonapi:"attr,author"`
+// 	Tags      []string          `json:"tags" jsonapi:"attr,tags"`
+// }
 
 type Meme struct {
-	Id        bson.ObjectId `json:"id" bson:"_id" jsonapi:"primary,memes"`
-	CreatedAt time.Time     `json:"createdAt" bson:"createdAt" jsonapi:"attr,createdAt"`
-	UpdatedAt time.Time     `json:"updatedAt" bson:"updatedAt" jsonapi:"attr,updatedAt"`
-	Title     string        `json:"title" jsonapi:"attr,title"`
-	Author    string        `json:"author" jsonapi:"attr,author"`
-	Tags      []string      `json:"tags" jsonapi:"attr,tags"`
+	ID bson.ObjectId `json:"-" bson:"_id"`
+	// CreatedAt time.Time `json:"createdAt"`
+	// UpdatedAt time.Time `json:"updatedAt"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	// Tags      []string  `json:"tags"`
+}
+
+// GetID to satisfy jsonapi.MarshalIdentifier interface
+func (m Meme) GetID() string {
+	return m.ID.Hex()
+}
+
+// SetID to satisfy jsonapi.UnmarshalIdentifier interface
+func (m *Meme) SetID(id bson.ObjectId) error {
+	m.ID = id
+	return nil
 }
