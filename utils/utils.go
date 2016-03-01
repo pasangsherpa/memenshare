96de60b65c4248d0bae1f06f9122da8a76e36e9c
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-01 00:00:56 -0800

    fix json encoding

---
+++ utils/utils.go
@@ -0,0 +1,16 @@
+package utils
+
+func Marshal(in interface{}) (interface{}, error) {
+	// data, err := jsonapi.Marshal(in)
+
+	// if err != nil {
+	// 	return make([]interface{}, 0), err
+	// }
+
+	// var result map[string]interface{}
+	// if err := json.Unmarshal(data, &result); err != nil {
+	// 	return make([]interface{}, 0), err
+	// }
+
+	return make([]interface{}, 0), nil
+}
