b4fd12b42b88058589f87015968ccf50717a4b75
Pasang Sherpa <pasang_sh@hotmail.com>
2016-03-01 22:55:33 -0800

    model fix

--- models/meme.go
+++ models/meme.go
@@ -1,24 +1,18 @@
 package models
 
-import "github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
+import (
+	"time"
 
-// type Meme struct {
-// 	Id        bson.ObjectId     `json:"id" bson:"_id" jsonapi:"primary,memes"`
-// 	Links     map[string]string `jsonapi:"links,top"`
-// 	CreatedAt time.Time         `json:"createdAt" bson:"createdAt" jsonapi:"attr,createdAt"`
-// 	UpdatedAt time.Time         `json:"updatedAt" bson:"updatedAt" jsonapi:"attr,updatedAt"`
-// 	Title     string            `json:"title" jsonapi:"attr,title"`
-// 	Author    string            `json:"author" jsonapi:"attr,author"`
-// 	Tags      []string          `json:"tags" jsonapi:"attr,tags"`
-// }
+	"github.com/pasangsherpa/memenshare/Godeps/_workspace/src/gopkg.in/mgo.v2/bson"
+)
 
 type Meme struct {
-	ID bson.ObjectId `json:"-" bson:"_id"`
-	// CreatedAt time.Time `json:"createdAt"`
-	// UpdatedAt time.Time `json:"updatedAt"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	// Tags      []string  `json:"tags"`
+	ID        bson.ObjectId `json:"-" bson:"_id"`
+	CreatedAt time.Time     `json:"createdAt"`
+	UpdatedAt time.Time     `json:"updatedAt"`
+	Title     string        `json:"title"`
+	Author    string        `json:"author"`
+	Tags      []string      `json:"tags"`
 }
 
 // GetID to satisfy jsonapi.MarshalIdentifier interface
